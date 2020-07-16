107bf7e700a47f47a82e1ed034d24050416e2667
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-07-16 21:04:10 +0530

    Posts from a Fake REST API displayed with the help of Retrofit in Android.

---
+++ JSONParsingExample/app/src/main/java/com/spikedshark/jsonparsingexample/JsonPlaceholderAPI.java
@@ -0,0 +1,12 @@
+package com.spikedshark.jsonparsingexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceholderAPI {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
